fd1bf75856e41ab02ee43c1db367b1831c75dd1d
Diego Arango <yodaad@gmail.com>
2024-04-05 15:17:59 -0500

    Functions file added

---
+++ src/06_functions.py
@@ -0,0 +1,17 @@
+# Functions in Python are blocks of code that only run when called. They can receive arguments and return values. 
+
+# Example 1 - Create a function that prints a message
+def my_print(text):
+    print(text * 2)
+
+# Call the function with an argument    
+my_print("This is my print within a function ")
+my_print("Hello ")
+
+# Example 2 - Create a function that returns a value
+def sum (a, b):
+    my_print(a+b)
+
+# Call the function with arguments
+sum(2, 4)
+sum(10, 4)
