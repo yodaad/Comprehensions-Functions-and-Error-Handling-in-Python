47e43d3d6fd8faeb29e83ad6252ab34b0834c746
Diego Arango <yodaad@gmail.com>
2024-04-16 12:25:29 -0500

    Scope file added

---
+++ src/09_scope.py
@@ -0,0 +1,19 @@
+"""Scope: The region of the program where a variable is recognized. Parameters and variables defined inside a function are not visible from outside the function. 
+Hence, they have a local scope. A variable declared outside a function has a global scope. Global variables can be used by functions within the program."""
+
+# Example 1 - Global variable
+price = 100
+
+# Function to increment the price
+def increment():
+    
+    # Local variable price inside the function increment 
+    price = 200
+    price = price + 10
+    print(price)
+    return price
+
+# Call the function
+print(price)
+price_2 = increment()
+print(price_2)
