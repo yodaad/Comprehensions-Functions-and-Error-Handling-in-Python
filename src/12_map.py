8461e8fb4b11c6ca80bc2ef4197dc1d04b4574ce
Diego Arango <yodaad@gmail.com>
2024-04-17 14:51:20 -0500

    Map file updated

--- src/12_map.py
+++ src/12_map.py
@@ -1,21 +1,29 @@
-# Map function is used to apply a function to all the elements of specified iterable and return map object.
+# Map function is used to apply a function to all the elements of specified iterable and return map object. Basically a map function executes the function on each item in the list.
 
 numbers = [1, 2, 3, 4]
-numbers_2 = []
+numbers2 = []
 
 for i in numbers:
-    numbers_2.append(i * 2)
+    numbers2.append(i * 2)
 
-numbers_3 = list(map(lambda i: i * 2, numbers))
+# Map function syntax - map(function, iterable). It returns a map object. Uses list to convert the map object to a list.
+numbers3 = list(map(lambda i: i * 2, numbers))
    
 print(numbers)
-print(numbers_2)
-print(numbers_3)
+print(numbers2)
+print(numbers3)
 
-numbers_1 = [1, 2, 3, 4]
-numbers_1_2 = [5, 6, 7]
+numbers1 = [1, 2, 3, 4]
+numbers1_2 = [5, 6, 7]
 
-print(numbers_1)
-print(numbers_1_2)
-result = list(map(lambda x, y: x + y, numbers_1, numbers_1_2))
+print(numbers1)
+print(numbers1_2)
+
+# Map function with multiple iterables and lambda function.
+result = list(map(lambda x, y: x + y, numbers1, numbers1_2))
 print(result)
+
+countries = ["col", "mex", "bol", "arg"]
+countries2 = list(map(lambda country: country.upper(), countries))
+print(countries)
+print(countries2)
