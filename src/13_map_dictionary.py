20d9876887d5f7df55a60f4994870e4467bbcd05
Diego Arango <yodaad@gmail.com>
2024-04-18 17:12:49 -0500

    Map Dictionary file added

---
+++ src/13_map_dictionary.py
@@ -0,0 +1,31 @@
+# Map function with dictionaries and lambda function.
+
+items = [
+    {
+        "product": "shirt",
+        "price": 100
+    },
+    {
+        "product": "pants",
+        "price": 300
+    },
+    {
+        "product": "pants 2",
+        "price": 200
+    }
+]
+
+prices = list(map(lambda item: item["price"], items))
+print(prices)
+
+products = list(map(lambda item: item["product"], items))
+print(products)
+
+def add_taxes(item):
+    item["taxes"] = item["price"] * .19
+    item["final_price"] = item["price"] + item["taxes"]
+    return item
+    
+
+new_items = list(map(add_taxes, items))
+print(new_items)
