61697fd3dd5f2efebc7b86bd9f1b533aca899e8f
Diego Arango <yodaad@gmail.com>
2024-04-22 18:24:06 -0500

    Filter file updated

--- src/15_filter.py
+++ src/15_filter.py
@@ -3,7 +3,13 @@ the elements that satisfy the condition."""
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-# Filter the numbers that are even from the list of numbers and store them in a new list called new_numbers using the filter function. 
+# Exmaple 1: Filter the numbers that are even from the list of numbers and store them in a new list called new_numbers using the filter function. 
 new_numbers = list(filter(lambda x: x %  2 == 0, numbers))
 print(new_numbers)
 print(numbers)
+
+letters = ['a', 'B', 'c', 'D', 'E', 'f']
+
+# Example 2: Filter the letters that are uppercase from the list of letters and store them in a new list called new_letters using the filter function.
+new_letters= list(filter(lambda x: x.isupper(), letters))
+print(new_letters)
