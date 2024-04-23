72fc7c3da8c2ad83e3697c03d5702abe4cd0b161
Diego Arango <yodaad@gmail.com>
2024-04-22 19:05:41 -0500

    Reduce file updated

--- src/17_reduce.py
+++ src/17_reduce.py
@@ -5,11 +5,11 @@ import functools
 
 numbers = [1, 2, 3, 4]
 
-# Example 1: Calculate the sum of all the numbers in the list using the reduce function.
+# Approach 1: Calculate the sum of all the numbers in the list using the reduce function.
 result = functools.reduce(lambda i, item: i + item, numbers)
 print(result)
 
-# Example 2: Calculate the difference of all the numbers in the list using the reduce function with a custom function.
+# Approach 2: Calculate the difference between the numbers in the list using the reduce function and a custom function.
 def accum(i, item):
     print("counter => ", i)
     print("item => ", item)
